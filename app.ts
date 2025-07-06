58622a0d5627cdc1bcebdb5a6e4d3eb9ad48e17c
SteamonAP <amoghpitale7@gmail.com>
2025-07-06 23:26:58 +0530

    small basic todo using deno

--- app.ts
+++ app.ts
@@ -1,8 +1,23 @@
-const text = 'This test should be written in file';
+// const text = 'This test should be written in file';
 
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
+// const encoder = new TextEncoder();
+// const data = encoder.encode(text);
 
-Deno.writeFile('message.txt',data).then(() => {
-    console.log('File data uploaded!');
+// Deno.writeFile('message.txt',data).then(() => {
+//     console.log('File data uploaded!');
+// });
+
+// import { serve } from "https://deno.land/std/http/server.ts";
+
+// serve((req) => {
+//     return new Response("Hello bhai log\n")
+// }, { port: 3000 });
+
+import { Application } from "https://deno.land/x/oak/mod.ts";
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello world\n";
 });
+
+await app.listen({ port: 8000 });
